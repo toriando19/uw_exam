9f08ffffce81518bb571cb78b65e45a6458373f9
viktoriaandonovska <viktoria_andonovska@hotmail.com>
2024-05-05 19:45:52 +0200

    "..."

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # uw_exam
+
+# Token: 
+# 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
+# https://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@github.com/toriando19/uw_exam.git
--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# Token: 
-# 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
-# https://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@github.com/toriando19/uw_exam.git
-
 from bottle import default_app, get, post, run
 import git
  
